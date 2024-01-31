6786d8b8b67adbcc017337ee226ba872b69d6c3a
michallow <michallo_w@o2.pl>
2024-01-31 09:52:58 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const createGalleryMarkup = items =>
+  items
+    .map(
+      item => `
+      <div class="gallery__item">
+          <a class="gallery__link" href="${item.original}">
+              <img 
+                  class="gallery__image" 
+                  src="${item.preview}" 
+                  data-source="${item.original}"
+                  alt="${item.description}"/>
+          </a>
+      </div>`
+    )
+    .join('');
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.innerHTML = addGalleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
