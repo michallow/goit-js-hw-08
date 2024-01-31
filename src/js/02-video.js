acfbb581d245cc2bafd50e427ea48081ee38398e
michallow <michallo_w@o2.pl>
2024-01-31 12:45:02 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,33 +4,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-const updateTimeInLocalStorage = throttle((currentTime) => {
-    localStorage.setItem('videoplayer-current-time', currentTime);
+const currentTime = throttle(data => {
+  localStorage.setItem('video-player-current-time', data.seconds);
 }, 1000);
 
-player.on('timeupdate', (event) => {
-    const currentTime = event.seconds;
-    updateTimeInLocalStorage(currentTime);
-});
+player.on('timeupdate', currentTime);
 
-const savedTime = localStorage.getItem('videoplayer-current-time');
-if (savedTime) {
-    player.setCurrentTime(parseFloat(savedTime));
-    console.log(savedTime);
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(parseFloat(storedTime));
 }
-
-player.on('play', () => {
-    console.log('played the video!');
-});
-
-player.getVideoTitle().then((title) => {
-    console.log('title:', title);
-});
-
-const onPlay = (timeupdate) => {
-    console.log('played the video!');
-};
-
-player.on('play', onPlay);
-
-player.off('play');
