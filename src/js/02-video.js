99ea36a3f4be3636fef7f950c89a95cd5cd7b00e
michallow <michallo_w@o2.pl>
2024-01-31 13:00:43 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,10 @@ import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Vimeo(iframe);
 
 const currentTime = throttle(data => {
-  localStorage.setItem('video-player-current-time', data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 }, 1000);
 
 player.on('timeupdate', currentTime);
